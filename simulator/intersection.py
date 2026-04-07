2ebe86dfe6953a0754dedbfe81eac5a03618442a
ReenuLK <reenugowda01@gmail.com>
2026-04-08 00:00:00 +0530

    refactor: modularize core simulator files and fix logic errors

--- simulator/intersection.py
+++ simulator/intersection.py
@@ -3,10 +3,7 @@ from collections import deque
 class Intersection:
     def __init__(self):
         self.lanes = {
-            "N": deque(),
-            "S": deque(),
-            "E": deque(),
-            "W": deque()
+            "N": deque(), "S": deque(), "E": deque(), "W": deque()
         }
         self.current_phase = 0  # 0: NS, 1: EW
         self.phase_duration = 0
@@ -16,20 +13,24 @@ class Intersection:
 
     def process_signal(self, k=2):
         moved = []
+        def has_emergency(dirs):
+            return any(v.is_emergency for d in dirs for v in self.lanes[d])
 
-        if self.current_phase == 0:
-            directions = ["N", "S"]
-        else:
-            directions = ["E", "W"]
+        ns_e = has_emergency(["N", "S"])
+        ew_e = has_emergency(["E", "W"])
+
+        if ns_e and not ew_e: directions = ["N", "S"]
+        elif ew_e and not ns_e: directions = ["E", "W"]
+        else: directions = ["N", "S"] if self.current_phase == 0 else ["E", "W"]
 
         for d in directions:
             for _ in range(min(k, len(self.lanes[d]))):
-                moved.append((d,self.lanes[d].popleft()))
-
+                moved.append((d, self.lanes[d].popleft()))
         return moved
 
+    # THIS IS THE MISSING METHOD
     def update_waiting(self):
-        for lane in self.lanes.values():
-            for v in lane:
-                v.wait_time += 1
-                v.fuel_consumed += 0.1
+        for direction in ["N", "S", "E", "W"]:
+            for vehicle in self.lanes[direction]:
+                vehicle.wait_time += 1
+                vehicle.fuel_consumed += 0.1
